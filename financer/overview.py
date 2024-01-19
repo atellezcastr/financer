f53b8036c1fb92984016e7fa586b5394b5f99473
atellezcastr <atellezcastr@mun.ca>
2024-01-19 20:12:19 -0330

    feat: added updating account behavior

--- financer/overview.py
+++ financer/overview.py
@@ -13,7 +13,10 @@ bp = Blueprint("overview", __name__)
 def index():
     db = get_db()
     # Assuming g.user['id'] contains the current user's ID
-    user_id = g.user["id"]
+    user_id = g.user["id"] if g.user else None
+    posts = []
+    accounts = []
+
     if user_id:
         posts = db.execute(
             "SELECT p.id, title, type_of_transaction, created, author_id, description_of_transaction, amount"
@@ -22,25 +25,12 @@ def index():
             " ORDER BY created DESC",
             (user_id,),
         ).fetchall()
-        accounts = db.execute(
-            """SELECT id, name_of_account, balance, type_of_account
-            FROM account
-            WHERE user_id = ?""",
-            (user_id,),
-        ).fetchall()
-        accounts = [
-            {
-                "id": account["id"],
-                "name_of_account": account["name_of_account"],
-                "balance": account["balance"],
-                "type_of_account": toa[account["type_of_account"]],
-            }
-            for account in accounts
-        ]
+        accounts = get_accounts(db, user_id)
     return render_template("overview/index.html", posts=posts, accounts=accounts)
 
 
 @bp.route("/create", methods=("GET", "POST"))
+@login_required
 def create():
     if request.method == "POST":
         type_of_transaction = int(request.form["type_of_transaction"])
@@ -48,6 +38,7 @@ def create():
         amount = float(request.form["amount"])
         comments = request.form["comments"]
         description_of_transaction = int(request.form["description_of_transaction"])
+        account_to_be_modified = request.form["accounts"]
         error = None
 
         ## TODO whats best? to keep them as a must? for now, yes
@@ -59,19 +50,79 @@ def create():
         if error:
             flash(error)
         else:
-            print(type_of_transaction)
-            db = get_db()
-            db.execute(
-                "INSERT into logs (author_id, title, type_of_transaction, amount, description_of_transaction, comments) VALUES (?,?,?,?,?,?)",
-                (
-                    g.user["id"],
-                    title,
-                    type_of_transaction,
-                    amount,
-                    description_of_transaction,
-                    comments,
-                ),
+            accounts = add_log_to_db(
+                get_db(),
+                g.user["id"],
+                title,
+                type_of_transaction,
+                amount,
+                description_of_transaction,
+                comments,
+                account_to_be_modified,
             )
-            db.commit()
-            return redirect(url_for("overview.index"))
-    return render_template("overview/create.html", toes=toes, tois=tois, tots=tots)
+            return render_template(
+                "overview/index.html", posts=[], accounts=accounts
+            )
+    accounts = get_accounts(get_db(), g.user["id"])
+    return render_template(
+        "overview/create.html", toes=toes, tois=tois, tots=tots, accounts=accounts
+    )
+
+
+def get_accounts(db, user_id):
+    query = """SELECT id, name_of_account, balance, type_of_account
+            FROM account
+            WHERE user_id = ?"""
+    accounts = db.execute(
+        query,
+        (user_id,),
+    ).fetchall()
+    accounts = [
+        {
+            "id": account["id"],
+            "name_of_account": account["name_of_account"],
+            "balance": account["balance"],
+            "type_of_account": toa[account["type_of_account"]],
+        }
+        for account in accounts
+    ]
+    return accounts
+
+
+Copy code
+def add_log_to_db(
+    db,
+    user,
+    title,
+    type_of_transaction,
+    amount,
+    description_of_transaction,
+    comments,
+    account_to_be_modified,
+):
+    add_log_query = "INSERT into logs (author_id, title, type_of_transaction, amount, description_of_transaction, comments) VALUES (?,?,?,?,?,?)"
+    transaction = db.execute(
+        add_log_query,
+        (
+            g.user["id"],
+            title,
+            type_of_transaction,
+            amount,
+            description_of_transaction,
+            comments,
+        ),
+    )
+
+    # Update account balance
+    affect_account_query = "UPDATE account SET balance = balance + ? WHERE id = ?"
+    affect_account_transaction = db.execute(
+        affect_account_query, (amount, account_to_be_modified)
+    )
+
+    # Fetch the updated account information after the balance update
+    accounts = get_accounts(db, g.user["id"])
+
+    # Commit changes to the database
+    db.commit()
+
+    return accounts
